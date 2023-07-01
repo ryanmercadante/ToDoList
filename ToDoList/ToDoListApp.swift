4b45fcbbe6c25489cda35dec355807ee38ea7f69
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-06-30 20:40:08 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ryan Mercadante on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
